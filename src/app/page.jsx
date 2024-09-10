9d55129790736c3bd7844ff49c8bf782226efcb9
maru2katabami <maru2katabami@gmail.com>
2024-09-10 16:30:51 +0900

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,20 +5,8 @@ import { Treemap } from "@/cmp/treemap"
 import { Header } from "@/cmp/header"
 
 export default function Page() {
-  useEffect(() => {
-
-    const disableZoom = (e) => e.preventDefault()
-    const parentElement = document.getElementById('home-container')
-    parentElement.addEventListener('touchmove', disableZoom, { passive: false })
-    parentElement.addEventListener('gesturestart', disableZoom )
-    return () => {
-      parentElement.removeEventListener('touchmove', disableZoom )
-      parentElement.removeEventListener('gesturestart', disableZoom )
-    }
-  }, [])
-
   return (
-    <main id="home-container">
+    <main>
       <Treemap/>
       <Header/>
     </main>
