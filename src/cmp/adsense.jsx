99462226e20dc071ed5680d39dd83ee309b32d16
maru2katabami <maru2katabami@gmail.com>
2024-09-21 22:22:14 +0900

    update

--- src/cmp/adsense.jsx
+++ src/cmp/adsense.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 export const Adsense = () => {
 
+  const [ width, setWidth ] = useState( 0 )
+
   useEffect(() => {
+    setWidth( window.innerWidth )
     try {
       ( window.adsbygoogle = window.adsbygoogle || []).push({})
     } catch ( err ) {
@@ -19,12 +22,12 @@ export const Adsense = () => {
         data-ad-slot="7363878258"></ins>
       <ins 
         className="adsbygoogle"
-        style={{ display: window.innerWidth >= 640 ? "block": "none", width: "100%", height: "100px" }}
+        style={{ display: width >= 640 ? "block": "none", width: "100%", height: "100px" }}
         data-ad-client="ca-pub-7513541440780811"
         data-ad-slot="5738261110"></ins>
       <ins 
         className="adsbygoogle"
-        style={{ display: window.innerWidth >= 1020 ? "block": "none", width: "100%", height: "100px" }}
+        style={{ display: width >= 1020 ? "block": "none", width: "100%", height: "100px" }}
         data-ad-client="ca-pub-7513541440780811"
         data-ad-slot="9518285104"></ins>
     </div>
