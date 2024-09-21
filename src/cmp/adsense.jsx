2f6c31a978dc4dc0600054c594f8ed869326f47a
maru2katabami <maru2katabami@gmail.com>
2024-09-21 22:25:40 +0900

    update

--- src/cmp/adsense.jsx
+++ src/cmp/adsense.jsx
@@ -17,17 +17,17 @@ export const Adsense = () => {
     <div className="w-full h-[100px] flex justify-around items-center bg-white">
       <ins
         className="adsbygoogle"
-        style={{ display: "block", width: "100%", height: "100px" }}
+        style={{ display: "block", width: "340px", height: "100px" }}
         data-ad-client="ca-pub-7513541440780811"
         data-ad-slot="7363878258"></ins>
       <ins 
         className="adsbygoogle"
-        style={{ display: width >= 640 ? "block": "none", width: "100%", height: "100px" }}
+        style={{ display: width >= 640 ? "block": "none", width: "340px", height: "100px" }}
         data-ad-client="ca-pub-7513541440780811"
         data-ad-slot="5738261110"></ins>
       <ins 
         className="adsbygoogle"
-        style={{ display: width >= 1020 ? "block": "none", width: "100%", height: "100px" }}
+        style={{ display: width >= 1020 ? "block": "none", width: "340px", height: "100px" }}
         data-ad-client="ca-pub-7513541440780811"
         data-ad-slot="9518285104"></ins>
     </div>
